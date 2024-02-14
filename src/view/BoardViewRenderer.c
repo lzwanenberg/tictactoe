d573b0804187ce2f954efdf21a0f921fbbde67c3
Luc Zwanenberg <luc.zwanenberg@gmail.com>
2024-02-14 12:16:22 +0100

    Add documentation for BoardViewRenderer

--- src/view/BoardViewRenderer.c
+++ src/view/BoardViewRenderer.c
@@ -26,12 +26,31 @@ static size_t calculate_character_position(int row, int column);
 static void set_character_at_position(char *buffer, int row_id, int col_id, char character);
 static void process_cell(struct BoardView *board, char *buffer, int row, int col);
 
+/**
+ * @brief Creates a buffer for representing the board as a string.
+ *
+ * Allocates memory for a character buffer to store the string representation
+ * of the board.
+ *
+ * @return A pointer to the allocated buffer. Caller is responsible for freeing
+ * the memory.
+ */
 char *create_board_string_buffer()
 {
   char *buffer = malloc(BOARD_STRING_SIZE * sizeof(char));
   return buffer;
 }
 
+/**
+ * @brief Renders the board view into the provided buffer.
+ *
+ * Fills the provided buffer with the string representation of the board.
+ *
+ * @param board Pointer to the BoardView structure representing the game board.
+ * @param buffer Pointer to the character buffer where the board view will be rendered.
+ *               The buffer must be preallocated by the caller and have a size of at least
+ *               BOARD_STRING_SIZE.
+ */
 void render_board_view(struct BoardView *board, char *buffer)
 {
   strcpy_s(buffer, BOARD_STRING_SIZE, EMPTY_BOARD);
