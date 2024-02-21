a648c58cd079be3822dce71e6e17cb3c784c83ac
Luc Zwanenberg <luc.zwanenberg@gmail.com>
2024-02-21 13:32:57 +0100

    Clean up GameView

--- src/view/GameView/GameView.c
+++ src/view/GameView/GameView.c
@@ -3,13 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "GameView.h"
+#include "ascii_art_title.h"
 #include "../BoardView/BoardView.h"
 
-#define ASCII_TITLE                                   \
-  " _______       ______           ______        \n"  \
-  "/_  __(_)___  /_  __/__ _____  /_  __/__  ___ \n"  \
-  " / / / / __/   / / / _ `/ __/   / / / _ \\/ -_)\n" \
-  "/_/ /_/\\__/   /_/  \\_,_/\\__/   /_/  \\___/\\__/ \n\n"
+void game_view__render_board(GameView *game_view, char *buffer);
 
 void game_view__initialize(GameView *game_view)
 {
@@ -20,14 +17,17 @@ void game_view__initialize(GameView *game_view)
 
 void game_view__render(GameView *game_view, char *buffer)
 {
-  strcat_s(buffer, OUTPUT_BUFFER_SIZE, ASCII_TITLE);
-
-  BoardView board_view;
-  board_view__initialize(&board_view, &game_view->board);
-  board_view__render(&board_view, buffer);
-
+  strcat_s(buffer, OUTPUT_BUFFER_SIZE, ASCII_ART_TITLE);
+  game_view__render_board(game_view, buffer);
   strcat_s(buffer, OUTPUT_BUFFER_SIZE, game_view->error);
   strcat_s(buffer, OUTPUT_BUFFER_SIZE, "\n");
   strcat_s(buffer, OUTPUT_BUFFER_SIZE, game_view->message);
   strcat_s(buffer, OUTPUT_BUFFER_SIZE, "\n");
+}
+
+static void game_view__render_board(GameView *game_view, char *buffer)
+{
+  BoardView board_view;
+  board_view__initialize(&board_view, &game_view->board);
+  board_view__render(&board_view, buffer);
 }
